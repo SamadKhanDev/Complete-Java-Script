b6059081224fa88ee3487bb24ac6e38018518012
Abdus Samad Khan <abdussamadkhan026@gmail.com>
2026-05-03 03:34:22 +0500

    added string comments

--- Basic-Part4.js
+++ Basic-Part4.js
@@ -8,37 +8,43 @@
 const name = "Samad"
 const steps = 50;
 
+//---------> Old Way
 console.log("Hellow "+name + " your rank is "+steps)
 
-// Template Literals:
+//---------> Modern Way using template literals
 console.log(`Hellow ${name} your rank is ${steps}`)
 
+//---------> String Object 
 const realName = new String('SammuBhaya');
 
 console.log(realName[4]);      //u
 console.log(realName.length);  //10
 
+//---------> .charAt()
 // At which index, Character is ? :
 console.log(realName.charAt(3)) //m
 
-// (starter, end-1)
-const newString = realName.substring(0, 5)
+//---------> .substring()
+const newString = realName.substring(0, 5)  // (starter, end-1)
 console.log(newString)           //Sammu
 
-// SammuBhaya
+//---------> .slice()
 const anotherString = realName.slice(0, 5);
-console.log(anotherString)       //Sammu
+console.log(anotherString)            //Sammu
 
+//---------> .trim()
 const trimString = "    Sammu    "
-console.log(trimString);           //     Sammu
-console.log(trimString.trim());    //Sammu
+console.log(trimString);              //     Sammu
+console.log(trimString.trim());       //Sammu
 
+//---------> .replace()
 const url = "https://github.com/SamadKhanDev"
-console.log(url.replace('mad', 'mmu')); //  https://github.com/SammuKhanDev
+console.log(url.replace('mad', 'mmu'));    //https://github.com/SammuKhanDev
 
+//---------> .includes()
+console.log(url.includes('github'));       //true
 
-console.log(url.includes('github'));  //true
-
-console.log(realName.split(""));
+//---------> .split()
+console.log(realName.split(""));           // ['S', 'a', 'm', 'm', 'u', 'B', 'h', 'a', 'y', 'a']
 
 
