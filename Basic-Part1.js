807f6bfda2b2135f66ccb0deed37af01e25ddfaa
Abdus Samad Khan <abdussamadkhan026@gmail.com>
2026-05-02 18:55:36 +0500

    Varibale concept added

---
+++ Basic-Part1.js
@@ -0,0 +1,25 @@
+/*
+
+---------->Declaring Variables
+
+const num=1
+let myEmail = "abdussamadkhan026@gmail.com"
+var passsord = "123456789"
+city = "karachi"
+let status; 
+
+---------->Checking modification levels
+
+// num = 2                        Can not re-assign in const
+myEmail = "sammu@demo.com"        Can re-assign in let
+password = "987654321"            can re-assign in var
+city = "Islamabad"                can re- assign 
+
+---------->Printing
+
+console.log(num);
+console.log(myEmail);
+console.log(passsord);
+console.log(city);
+console.log(status);           //undefined
+*/
