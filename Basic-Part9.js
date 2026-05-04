89dfe8345871ae0eb523b1e7bf4aa5cd873c5f8a
Abdus Samad Khan <abdussamadkhan026@gmail.com>
2026-05-04 16:08:08 +0500

    added object destructuring

---
+++ Basic-Part9.js
@@ -0,0 +1,16 @@
+//---------> Desctructuring Obects
+
+/*
+const product = {
+    productName: "watch",
+    price: "Rs200",
+    madeIn: "China"
+}
+product.productName
+
+same as,
+
+const {productName: prdname} = product
+console.log(prdname);      //watch
+
+*/
