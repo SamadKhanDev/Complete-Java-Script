d51fc340cfb96de541af29cc2785b22101b5d7c6
Abdus Samad Khan <abdussamadkhan026@gmail.com>
2026-05-04 19:59:33 +0500

    added functions basics

---
+++ Basic-Part10.js
@@ -0,0 +1,35 @@
+//---------> Functions and Parameter's
+
+/*
+function guessAge(){
+    console.log("21")
+    console.log("22")
+    console.log("23")
+    console.log("24")
+}
+
+// reference
+guessAge
+
+// exectution
+guessAge()
+
+//---------> 
+
+function addNum(num1, num2){        //Parameters
+    var reuslt = num1+num2
+
+console.log(reuslt);
+}
+
+addNum(2,2);  //4                  //Arguments when call
+
+
+
+function login(username){
+    return `${username} login Successfuly!`
+}
+
+console.log(login("samad"));      //Samad login Successfuly!
+console.log(login());             //undefined login Successfuly!
+*/
