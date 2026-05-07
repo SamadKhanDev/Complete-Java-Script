109bdbb7346c8f46c7a8135fb0478dfa156a4556
Abdus Samad Khan <abdussamadkhan026@gmail.com>
2026-05-08 01:40:17 +0500

    added scopes

---
+++ Basic Part12.js
@@ -0,0 +1,33 @@
+/*
+//---------> Scopes: Involves Concept of Local & Global Scope
+
+{} --> Scope
+
+const a = 10;
+let b = 20;
+var c = 30;
+
+console.log(a)   //10
+console.log(b)   //20
+console.log(c)   //30
+
+//---------> Let -> Local Scope
+
+if(true){
+let a = 10;
+console.log("Inner Value: "+a)   /10
+}
+
+let a = 200;
+console.log("Outer Value: " +a)  //200
+
+//---------> var -> Global Scope
+
+if(true){
+var b = 20;
+console.log("Inner Value: "+b)    //20
+}
+
+var b =400;
+console.log("Outer Value: "+b)    //400
+*/
